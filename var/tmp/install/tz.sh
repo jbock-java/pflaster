1650b374cd74107ad432f419205f7acf80527a62
jbock <jbock-java@gmx.de>
2026-03-20 17:29:27 +0100

    luks fail fast

--- var/tmp/install/tz.sh
+++ var/tmp/install/tz.sh
@@ -3,18 +3,22 @@
 list_prefix() {
   local prefix result
   prefix="$1"
-  if (( ${#prefix} <= 2 )); then
+  if (( ${#prefix} == 0 )); then
     result=$(timedatectl list-timezones | sed -E 's@^(...).*@\1@' | sort -u | tr '\n' ' ')
     result=${result% }
-    echo "$result"
+  elif (( ${#prefix} <= 2 )); then
+    result=$(timedatectl list-timezones | sed -E 's@^(...).*@\1@' | grep -i "^$prefix" | sort -u | tr '\n' ' ')
+    result=${result% }
+    if (( ${#result} < 74 )) then
+      result=$(timedatectl list-timezones | grep -i "^$prefix" | tr '\n' ' ')
+      result=${result% }
+    fi
   else
     result=$(timedatectl list-timezones | grep -i "^$prefix" | tr '\n' ' ')
     result=${result% }
-    if [[ $result ]]; then
-      echo "$result"
-    else
-      echo "-- no match --"
-    fi
+  fi
+  if [[ $result ]]; then
+    echo "$result"
   fi
 }
 
